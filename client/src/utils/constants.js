9a16bb1e7b3afa314346cfd7cbc941ede030f0f4
nehakgg <107543046+nehakgg@users.noreply.github.com>
2024-03-24 11:11:40 +0530

    added server api

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,3 +1,3 @@
 export const LOGO_URL = 'https://upload.wikimedia.org/wikipedia/en/b/b5/National_Institute_of_Technology%2C_Patna_Logo.png';
 // export const BASE_URL =  'https://alumni-server-alpha.vercel.app/api/v1';
-export const BASE_URL = "http://localhost:4000/api/v1";
+export const BASE_URL = "https://alumuni-track-4.vercel.app/api/v1";
